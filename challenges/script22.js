40be47180e17310a4532fedaf0e910f81c8ad578
Guo Tao <goto99@gmail.com>
2023-04-16 14:57:26 +0800

    Finished challenges, slideshow project and advanced converter project.

---
+++ challenges/script22.js
@@ -0,0 +1,8 @@
+(function(){
+    const btn = document.querySelector('button');
+    const para = document.querySelector('p');
+    btn.addEventListener('click', function(){
+        para.style.color = 'green';
+    });
+})();
+
